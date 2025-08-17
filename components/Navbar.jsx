364ac7974d91a83a7fb60af9543c53e79d131f70
unknown <ajincraju@gmail.com>
2025-08-17 16:48:15 +0530

    Initial Commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Navbar () {
+  return (
+    <div className='h-18 items-center justify-center flex bg-gray-300'>
+      <ul className='flex space-x-32 text-xl'>
+        <li>Projects</li>
+        <li>About Me</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  )
+}
